b5f02457d93b676935af64fb62f0d248ce48f643
eh-lee <fornewstack@daum.net>
2023-02-16 11:41:39 +0900

    CT 14 commit

---
+++ 9~16/js/eh/eh-13.js
@@ -0,0 +1,47 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12901
+//2016년
+
+
+//드디어 .map() 을 써 보자
+//map()은 지도처럼 쫙 한 번 돌리는 겨!
+//map()실습
+
+// //1
+// let arr1 = [3, 65, 432, 4];
+// console.log(arr1.map(x => x *2)); //> [6, 130, 864, 8]
+
+
+
+// //2
+// var kvArray = [
+//   {key:1, value:10},
+//   {key:2, value:20},
+//   {key:3, value: 30}];
+
+// var reformattedArray = kvArray.map(function(obj){
+//   var rObj = {};
+//   rObj[obj.key] = obj.value;
+//   //??..객체의 인덱스를 표현하는 방법?
+//   return rObj;
+// });
+
+// console.log(reformattedArray); //> [{1: 10}, {2: 20}, {3: 30}]
+
+
+
+//그리고 toString()?
+
+
+ 
+//하...
+
+
+
+//3
+//일단 할 수 있는 것을 하기
+
+let day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+let month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+//몰러 이건 일단 패스..
---
+++ 9~16/js/eh/eh-14.js
@@ -0,0 +1,81 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12910
+//나누어 떨어지는 숫자 배열
+
+
+
+//IDEA
+//오름차순
+//arr.push()
+
+
+// let arr = [5, 9, 7, 10];
+// let divisor = 5;
+
+
+// let arr = [5, 9, 7, 10];
+// let divisor = 1;
+// let answer = [];
+
+// let arr = [3, 2, 6]
+// let divisor = 10;
+
+// for(let i = 0; i< arr.length; i++){
+//     if(arr[i]%divisor == 0 ? answer.push(arr[i]) : null ){
+//     } 
+// }
+// console.log(answer); //>[5, 9, 7, 10] 왜 바로 되지;;
+
+
+// if(answer.length == 0){
+//     answer.push(-1);
+// }
+// console.log(answer);
+
+
+
+
+
+
+// //이제 + 오름차순 정렬
+// // answer.sort() 이게 다네유..
+// //근데 저걸로 하면 안 되구 .sort(funciton(a,b) {return a-b;})
+
+// let arr = [9, 5, 10, 7];
+// let divisor = 5;
+// let answer = [];
+
+// for(let i = 0; i< arr.length; i++){
+//     if(arr[i]%divisor == 0 ? answer.push(arr[i]) : null ){
+//     } 
+// }
+// // console.log(answer.sort()); //아 이거 유니코드 어쩌구라네..
+
+// console.log(answer.sort(function(a, b)  {return a - b;}))
+
+
+// if(answer.length == 0){
+//     answer.push(-1);
+// }
+
+
+// // 끝.
+
+
+
+//최종
+
+function solution(arr, divisor) {
+    var answer = [];
+    
+    for(let i = 0; i< arr.length; i++){
+        if(arr[i]%divisor == 0 ? answer.push(arr[i]) : null ){
+        } 
+    }
+    answer = answer.sort(function(a, b)  {return a - b;});
+    
+    if(answer.length == 0){
+        answer.push(-1);
+    }
+    
+    return answer;
+}
