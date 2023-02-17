eb6751c24481edb2ae3e411cef46ae607a30aaab
eh-lee <fornewstack@daum.net>
2023-02-17 13:35:01 +0900

    CT 18 commit

--- 17~24/js/eh/eh-18.js
+++ 17~24/js/eh/eh-18.js
@@ -3,4 +3,30 @@
 
 
 //슈도코딩
-//
+//seoul = [];
+// `"${seoul[1]}의
+//"김서방은 x에 있다"
+//x = Kim이라는 문자열이 들어간 배열의 인덱스 번호.
+//var answer = '김서방은 에 있다';
+
+
+// function solution(seoul){
+//     // var answer = '';
+//     // return answer;
+//     seoul.indexOf("Kim")
+//     console.log(seoul.indexOf("Kim")); //> 1 인덱스 번호 찍힘.
+// }
+
+
+
+//2 ex) solution(["jane", "Kim"]);
+function solution(seoul){
+    var answer = '';
+    let x = seoul.indexOf("Kim")
+    // return answer;
+    // console.log(x); //> 1
+
+    // answer = "김서방은 x에 있다";
+    answer = "김서방은 "+x+"에 있다";
+    return answer;
+}
